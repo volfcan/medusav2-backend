bb5699ab6219efb24faeb4284b491c2520cf8cb0
volfcan <20183360068@ogr.akdeniz.edu.tr>
2024-08-22 14:20:04 +0300

    +

--- medusa-config.js
+++ medusa-config.js
@@ -1,16 +1,37 @@
-import { loadEnv, defineConfig } from '@medusajs/utils'
+import { loadEnv, defineConfig } from "@medusajs/utils";
 
-loadEnv(process.env.NODE_ENV, process.cwd())
+loadEnv(process.env.NODE_ENV, process.cwd());
+
+const plugins = [
+  // ...
+  {
+    resolve: "@medusajs/admin",
+    /** @type {import('@medusajs/admin').PluginOptions} */
+    options: {
+      autoRebuild: true,
+      // other options...
+    },
+  },
+];
 
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    database_extra:
+      process.env.NODE_ENV !== "development"
+        ? {
+            ssl: {
+              rejectUnauthorized: false,
+            },
+          }
+        : {},
+
     http: {
       storeCors: process.env.STORE_CORS,
       adminCors: process.env.ADMIN_CORS,
       authCors: process.env.AUTH_CORS,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
-  }
-})
+    },
+  },
+});
